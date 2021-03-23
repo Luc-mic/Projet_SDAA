86aee4d244586803c56f865b7f7b8d11f4e34e49
Nino <dauvier.nino@gmail.com>
2021-03-23 09:14:32 +0100

    main mis a jour, gitignore mis a jour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.test_graph import *
 from src.test_graph_generation import *
+from src.temps_generation import *
 
 
-test_graph()
-test_graph_generation()
+#test_graph()
+#test_graph_generation()
+print(time_generate_graph(10, 100, 10, 1))
